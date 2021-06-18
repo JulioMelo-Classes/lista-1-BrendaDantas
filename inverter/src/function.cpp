8aa8e70d4faaf847198b351dcce921bf5e3f9bf6
BrendaDantas <brendasod1998@gmail.com>
2021-06-18 13:16:09 +0000

    Tarefa concluída.

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -6,15 +6,14 @@ using std::iter_swap;
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr)
 {
-  std::string aux1, aux2;
+  int cont1, cont2 = arr.size();
 
-  for (int cont1 = 0; cont1 < arr.size(); cont1++) 
+  while (cont1 < cont2) 
   {
-    for (int cont2 = arr.size(); cont2 > 0; cont2--) 
+    for (cont1 = 0; cont1 < cont2; cont1++)
     {
-      aux1 = arr.at(cont1);
-      aux2 = arr.at(cont2-1);
       arr.at(cont1).swap(arr.at(cont2-1));
+      cont2--;
     }
   }
 }
