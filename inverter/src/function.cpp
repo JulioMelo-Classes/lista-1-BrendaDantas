bf900efab92ba5c3b62aa7bdc02554cf53e22955
BrendaDantas <brendasod1998@gmail.com>
2021-06-18 02:21:37 +0000

    Código com erro.

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,14 +1,18 @@
-#include "function.h"
 
+#include <array>
 #include <iterator>
 using std::iter_swap;
 
-/*! 
- * Reverse de order of elements inside the array.
- * @param arr Reference to the array with the values.
- */
-template <size_t SIZE>
-void reverse( std::array< std::string, SIZE > & arr )
+template <std::size_t SIZE>
+void reverse( std::array< std::string, SIZE > & arr)
 {
-    /*codigo*/
+    std::string aux1, aux2;
+    for (int cont1 = 0; cont1 < arr.size(); cont1++) 
+    {
+      for (int cont2 = arr.size(); cont2 > 0; cont2--) {
+        aux1 = arr.at(cont1);
+        aux2 = arr.at(cont2);
+        arr.at(cont1).swap(arr.at(cont2));
+      }
+    }
 }
