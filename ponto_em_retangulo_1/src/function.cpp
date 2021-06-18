b390b7915918b85c11893a0b1238251977ea4513
BrendaDantas <brendasod1998@gmail.com>
2021-06-18 20:54:11 +0000

    Código entrando em loop.

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -1,13 +1,20 @@
 #include "function.h"
 
-/*! 
- * Verifica se um ponto está dentro de um retângulo.
- */
-
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-    // TODO: Coloque aqui seu código.
+  if (P.x > IE.x && P.x < SD.x && P.y > IE.y && P.y < SD.y)
+  {
+    return location_t::INSIDE;
+  }
 
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
+  else if (P.x < IE.x || P.x > SD.x || P.y < IE.y || P.y > SD.y) 
+  { 
     return location_t::OUTSIDE;
+  }
+
+  else if (P.x == IE.x || P.x == SD.x || P.y == IE.y || P.y == SD.y) 
+  {
+    return location_t::BORDER;
+  }
+        
 }
