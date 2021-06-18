163a7579f12ffb016f827ec89953cf628390cf81
BrendaDantas <brendasod1998@gmail.com>
2021-06-18 02:24:54 +0000

    Código com erro.

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -6,13 +6,15 @@ using std::iter_swap;
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr)
 {
-    std::string aux1, aux2;
-    for (int cont1 = 0; cont1 < arr.size(); cont1++) 
+  std::string aux1, aux2;
+
+  for (int cont1 = 0; cont1 < arr.size(); cont1++) 
+  {
+    for (int cont2 = arr.size(); cont2 > 0; cont2--) 
     {
-      for (int cont2 = arr.size(); cont2 > 0; cont2--) {
-        aux1 = arr.at(cont1);
-        aux2 = arr.at(cont2);
-        arr.at(cont1).swap(arr.at(cont2));
-      }
+      aux1 = arr.at(cont1);
+      aux2 = arr.at(cont2);
+      arr.at(cont1).swap(arr.at(cont2));
     }
+  }
 }
