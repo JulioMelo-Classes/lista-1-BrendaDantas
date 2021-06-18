80654bb99dd4bbc9eee3f844b9d3ab7d209dcf0b
BrendaDantas <brendasod1998@gmail.com>
2021-06-18 21:14:44 +0000

    Código entrando em looping.

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -2,17 +2,19 @@
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-  if (P.x > IE.x && P.x < SD.x && P.y > IE.y && P.y < SD.y)
+  if ((P.x >= IE.x || P.x <= SD.x) && (P.y >= IE.y || P.y <= SD.y)) 
   {
-    return location_t::INSIDE;
+ 
+      return location_t::INSIDE;
+    
   }
 
-  else if (P.x < IE.x || P.x > SD.x || P.y < IE.y || P.y > SD.y) 
+  else if ((P.x < IE.x || P.x > SD.x) && (P.y < IE.y || P.y > SD.y)) 
   { 
     return location_t::OUTSIDE;
   }
 
-  else if (P.x == IE.x || P.x == SD.x || P.y == IE.y || P.y == SD.y) 
+  else if ((P.x == IE.x || P.x == SD.x) && (P.y == IE.y || P.y == SD.y)) 
   {
     return location_t::BORDER;
   }
