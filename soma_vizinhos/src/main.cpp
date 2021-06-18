966467ebd13a9d19deb61821db50e37a76f39b26
BrendaDantas <brendasod1998@gmail.com>
2021-06-18 15:05:41 +0000

    Tarefa concluída.

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -7,12 +7,15 @@ int main (void)
 {
   int valor1, valor2, cont = 0, soma = 0;
 
-  
   while (cin >> std::ws >> valor1 && cin >> std::ws >> valor2) 
   {
 
     if (valor2 > -1000 && valor2 < 1000) 
     {
+      if (valor2 == 0) 
+      {
+        soma = valor1; 
+      }
       if (valor2 > 0) 
       {
         while (cont < valor2) 
@@ -21,21 +24,21 @@ int main (void)
           valor1 = valor1 + 1;
           cont++;
         }
-        cout << soma << endl;
-        cont = 0;
-        soma = 0;
       }
 
       else
+      {
         while (cont > valor2) 
         {
           soma = soma + valor1;
           valor1 = valor1 - 1;
           cont--;
         }
-        cout << soma << endl;
-        cont = 0;
-        soma = 0;
+      }
+
+    cout << soma << endl;
+    cont = 0;
+    soma = 0;
 
     }
   }
