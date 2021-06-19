42dd9c38d0df99e884f6e28aed7fe616729b4a8f
BrendaDantas <brendasod1998@gmail.com>
2021-06-19 15:06:15 +0000

    O código falhou em 4 testes.

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -7,11 +7,13 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
 
   for (int cont = 0; cont < n; cont++) 
   {
-    if (n == 0 || n == 1) 
+    if (cont == 0 || cont == 1) 
     {
       sequencia.push_back(1);
     }
-    sequencia.push_back(sequencia.at(cont-1) + sequencia.at(cont-2));
+    else {
+      sequencia.push_back(sequencia.at(cont-1) + sequencia.at(cont-2));
+    }
   }
     return sequencia;
 }
