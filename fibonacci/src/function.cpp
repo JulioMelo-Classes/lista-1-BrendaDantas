35e32b9fc8e2111322ea19c64e873170b9976f09
BrendaDantas <brendasod98@gmail.com>
2021-06-19 22:34:18 +0000

    Tarefa concluido.

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -4,9 +4,18 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
 {
 
   std::vector<unsigned int> sequencia;
+  
+  if(n == 1){
+    return sequencia;  
+  }
 
   for (int cont = 0; cont < n; cont++) 
   {
+    if (sequencia.size() >=1 && sequencia.back() >= n)
+    {
+      sequencia.pop_back();
+      return sequencia;
+    }
     if (cont == 0 || cont == 1) 
     {
       sequencia.push_back(1);
@@ -15,5 +24,4 @@ std::vector<unsigned int> fib_below_n( unsigned int n )
       sequencia.push_back(sequencia.at(cont-1) + sequencia.at(cont-2));
     }
   }
-    return sequencia;
 }
