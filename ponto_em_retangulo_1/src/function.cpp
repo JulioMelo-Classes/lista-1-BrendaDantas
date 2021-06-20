03464b82791662c9ea8b925e2e5f04b8dcf55530
BrendaDantas <brendasod98@gmail.com>
2021-06-20 19:41:43 +0000

    Tarefa concluída.

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -4,9 +4,7 @@ location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
   if (P.x > IE.x && P.x < SD.x && P.y > IE.y && P.y < SD.y) 
   {
- 
-    return INSIDE;
-    
+    return INSIDE;    
   }
 
   if ((P.x == IE.x && P.y == IE.y) || (P.x == SD.x && P.y == SD.y) || (P.x == SD.x && P.y == IE.y) || (P.x == IE.x && P.y == SD.y) || (P.x == IE.x && P.y > IE.y && P.y < SD.y) || (P.x == SD.x && P.y > IE.y && P.y < SD.y) || (P.x > IE.x && P.x < SD.x && P.y == IE.y) || (P.x > IE.x && P.x < SD.x && P.y == SD.y) )
