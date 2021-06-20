492d938f5e8cd7ed5bd91bc9db6e24371620fb7f
BrendaDantas <brendasod98@gmail.com>
2021-06-20 18:04:15 +0000

    Código modificado mas ainda entrando em looping.

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -9,9 +9,8 @@ location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
     
   }
 
-  if ((P.x >= IE.x && P.x <= SD.x) || (P.y >= IE.y && P.y <= SD.y))
+  if ((P.x == IE.x && P.y == IE.y) || (P.x == SD.x && P.y == SD.y) || (P.x == SD.x && P.y == IE.y) || (P.x == IE.x && P.y == SD.y) || (P.x == IE.x && P.y > IE.y && P.y < SD.y) || (P.x == SD.x && P.y > IE.y && P.y < SD.y) || (P.x > IE.x && P.x < SD.x && P.y == IE.y) || (P.x > IE.x && P.x < SD.x && P.y == SD.y) )
   {
-
     return BORDER;
   }
 
