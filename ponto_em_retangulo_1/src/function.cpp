079b05097b4e520421b41d1b6f16e774e6a00e40
BrendaDantas <brendasod98@gmail.com>
2021-06-20 03:25:07 +0000

    Tive progresso na lógica do código porém ele continua entrando em looping nos testes.

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -2,21 +2,22 @@
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-  if ((P.x >= IE.x || P.x <= SD.x) && (P.y >= IE.y || P.y <= SD.y)) 
+  if (P.x > IE.x && P.x < SD.x && P.y > IE.y && P.y < SD.y) 
   {
  
-      return location_t::INSIDE;
+    return INSIDE;
     
   }
 
-  else if ((P.x < IE.x || P.x > SD.x) && (P.y < IE.y || P.y > SD.y)) 
-  { 
-    return location_t::OUTSIDE;
+  if ((P.x >= IE.x && P.x <= SD.x) || (P.y >= IE.y && P.y <= SD.y))
+  {
+
+    return BORDER;
   }
 
-  else if ((P.x == IE.x || P.x == SD.x) && (P.y == IE.y || P.y == SD.y)) 
-  {
-    return location_t::BORDER;
+  else  
+  { 
+    return OUTSIDE;
   }
         
 }
